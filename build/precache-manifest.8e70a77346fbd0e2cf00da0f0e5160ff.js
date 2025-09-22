da3a9b732e4b12b3e64e0f8dc68d35b90a01f3b8
vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
2025-09-21 20:48:09 -0500

    build

---
+++ build/precache-manifest.8e70a77346fbd0e2cf00da0f0e5160ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1ed659b6cb26119eb4a",
+    "url": "/autoecu/static/css/main.f39dfb5d.chunk.css"
+  },
+  {
+    "revision": "f1ed659b6cb26119eb4a",
+    "url": "/autoecu/static/js/main.f1ed659b.chunk.js"
+  },
+  {
+    "revision": "796b8347d9a9eaaeb9fb",
+    "url": "/autoecu/static/js/1.796b8347.chunk.js"
+  },
+  {
+    "revision": "99df6ae365fdd76a2f46",
+    "url": "/autoecu/static/js/runtime~main.99df6ae3.js"
+  },
+  {
+    "revision": "e6c3f4fd529da495bd7c0130d230f5f7",
+    "url": "/autoecu/0f0004c8d66c121697a0.worker.js"
+  },
+  {
+    "revision": "eb4b5021c992357578a123a594720cde",
+    "url": "/autoecu/index.html"
+  }
+];
